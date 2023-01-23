59eceff26c696a8a55477e4937c611cf6e987808
GodwinMaKyao <godwinnamwel@gmail.com>
2023-01-23 09:41:44 +0300

    sign changes

--- src/utills/APIRoute.js
+++ src/utills/APIRoute.js
@@ -1,5 +1,4 @@
-//  export const host = "https://animaltracking.patrickmamsery.works";
-export const host = "http://localhost:5003";
+ export const host = "https://animaltracking.patrickmamsery.works";
 
  export const signRoute = `${host}/api/v1/sign`
 export const loginRoute = `${host}/api/v1/login`
