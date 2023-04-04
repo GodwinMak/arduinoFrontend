b45676b83f98e8a6a86ca9a31e21850e28928441
GodwinMaKyao <godwinnamwel@gmail.com>
2023-04-04 11:47:25 +0300

    access token

--- src/utills/APIRoute.js
+++ src/utills/APIRoute.js
@@ -1,5 +1,5 @@
-//  export const host = "https://animaltracking.patrickmamsery.works";
- export const host = "http://localhost:5003";
+ export const host = "https://animaltracking.patrickmamsery.works";
+//  export const host = "http://localhost:5003";
 
 
  export const signRoute = `${host}/api/v1/sign`
